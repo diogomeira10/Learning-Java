1918a2172d8df9b19a5d81c9e70c2ff2791e9ccd
Diogo Meira <diogomeira81@gmail.com>
2024-10-02 21:41:33 +0100

    Functions, methods and classes

---
+++ Main.java
@@ -0,0 +1,20 @@
+//Functions
+
+// Define the type of value the functions returns: 
+
+//main is the entry point of the program
+//functions must belong to a class
+//functions that belong to a class are generally called methods.
+// classes are used to organize our code
+//Every Java program should have at least one class that contains the main function
+
+// In Java , all the classes and methods should have an access modifier. Wich is a keyword that determines if other classes or methods can access these classes and methods
+
+//naming classes with Pascal is a convention, 
+//naming methods in camelCase is also a convention;
+
+public class Main { //this is the class
+    public void main() { // main function
+
+    }
+}
